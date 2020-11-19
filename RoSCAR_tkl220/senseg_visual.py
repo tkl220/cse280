46469a1642374f75d4a84d79c9ce840a98caf3db
T. Kingsley Leighton <tkl220@lehigh.edu>
2020-11-19 14:45:07 -0500

    python script to visualize sensor segments

---
+++ RoSCAR_tkl220/senseg_visual.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import numpy as np
+import pylab as pl
+from matplotlib import collections as mc
+import csv
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    lines = []
+    with open('senseg_old.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            segment = [(row[0], row[1]), (row[2], row[3])]
+            lines.append(segment)
+        lc = mc.LineCollection(lines, linewidths=2)
+        fig, ax = pl.subplots()
+        ax.add_collection(lc)
+        ax.autoscale()
+        ax.margins(0.1)
+        fig.show()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
