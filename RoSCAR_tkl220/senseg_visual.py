b9e59e193f60742285ed4ff32af3f5c38df91233
Walker Finlay <walker.finlay@gmail.com>
2020-11-19 16:21:19 -0500

    just cosmetics

--- RoSCAR_tkl220/senseg_visual.py
+++ RoSCAR_tkl220/senseg_visual.py
@@ -17,22 +17,26 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    lines = []
+    walls = []
+    sensors = []
     with open('track.csv', 'r') as track_csv:
         csv_reader = csv.reader(track_csv, delimiter=',')
         for row in csv_reader:
             segment = [(row[0], row[1]), (row[2], row[3])]
-            lines.append(segment)
+            walls.append(segment)
     with open('sensor_segments.csv', 'r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             segment = [(row[0], row[1]), (row[2], row[3])]
-            lines.append(segment)
-        lc = mc.LineCollection(lines, linewidths=2)
+            sensors.append(segment)
+        wc = mc.LineCollection(walls, linewidths=2)
+        sc = mc.LineCollection(sensors, linewidths=0.5, colors='#ff4545')
         fig, ax = pl.subplots()
-        ax.add_collection(lc)
+        ax.add_collection(wc)
+        ax.add_collection(sc)
         ax.autoscale()
         ax.margins(0.1)
+        ax.grid(True)
         fig.show()
         matplotlib.pyplot.show()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
